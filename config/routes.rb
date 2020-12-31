ed25407b469ef74ff994387262e1a2caaa956b86
rahul chalamarla <rahulram9993@gmail.com>
2020-12-31 14:55:45 +0530

    custom portfolio route added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get '/about', to: 'page#about'
   get '/contact', to: 'page#contact'
 
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: :portfolio_show
   resources :blogs
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
