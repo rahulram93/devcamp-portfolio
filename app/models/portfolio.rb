2f7f5b7dd64719fa6fba05ddfc507f5ce93ac386
rahul chalamarla <rahulram9993@gmail.com>
2021-01-01 19:49:59 +0530

    Implemented concern for image data url

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   scope :ruby_on_rails, -> { where(:subtitle => "ruby on rails") }
@@ -6,7 +7,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "https://via.placeholder.com/600x400"
-    self.thumb_image ||= "https://via.placeholder.com/350x200"
+    self.main_image ||= Placeholder.image_generator(600,400)
+    self.thumb_image ||= Placeholder.image_generator(350,200)
   end
 end
