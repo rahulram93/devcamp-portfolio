a324095a21300443bedc0f60a8580457c9411fe6
rahul chalamarla <rahulram9993@gmail.com>
2021-02-27 11:35:11 +0530

    Auth user name field added for authentication

--- db/migrate/20210208164330_devise_create_users.rb
+++ db/migrate/20210208164330_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :encrypted_password, null: false, default: ""
 
       ## Custom fields
-      t.string name
+      t.string :name
 
       ## Recoverable
       t.string   :reset_password_token
